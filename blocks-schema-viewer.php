aa32f696bfa2c1807577ce289d608313269f3717
Brandon Zhang <gwonzhang@gmail.com>
2025-01-24 20:11:45 +0700

    update plugin meta and commit build/ dir

--- blocks-schema-viewer.php
+++ blocks-schema-viewer.php
@@ -5,12 +5,13 @@
  * Version:           0.1.0
  * Requires at least: 6.7
  * Requires PHP:      7.4
- * Author:            heybran
+ * Author:            Brandon Zhang
+ * Author URI:        https://github.com/heybran
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       blocks-schema-viewer
  *
- * @package BlocksSchemaViewer
+ * @package           Blocks Schema Viewer
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
